dff36be1eb3a91f89a152ac699a28fd33a4cd044
Pires C <krlos.pires@gmail.com>
2017-05-15 13:02:14 -0400

    Creacion del token en la API, interaccion contra api usando tokens

--- api/env/app/app.py
+++ api/env/app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask,jsonify,json,request
 from flask_mysqldb import MySQL 
+import uuid
 
 app= Flask(__name__)
 
@@ -12,161 +13,205 @@ mysql= MySQL(app)
 #Metodo para traer todos los productos 
 @app.route("/productos/", methods=['GET'])
 def get_products():
+	sesion = request.headers['Sesion']
 	cur=mysql.connection.cursor()
-	cur.execute('SELECT * FROM producto')
+	estado_sesion = cur.execute('SELECT * FROM token where token_cod= %s', [sesion])
+	cur.fetchall()
+	if estado_sesion==1:
+		cur.execute('SELECT * FROM producto')
+		respuesta= cur.fetchall() 
+		product_list=[]
+		for i in range (0,len(respuesta)):
+			item={
+				'id':respuesta[i][0],
+				'id_categoria': respuesta[i][1],
+				'nombre':respuesta[i][2],
+				'descripcion':respuesta[i][3],
+				'cantidad':respuesta[i][4],
+				'precio':respuesta[i][5],
+				'imagen':respuesta[i][6]}
+			product_list.append(item)
 
-	respuesta= cur.fetchall() 
-	product_list=[]
-	for i in range (0,len(respuesta)):
-		item={
-			'id':respuesta[i][0],
-			'id_categoria': respuesta[i][1],
-			'nombre':respuesta[i][2],
-			'descripcion':respuesta[i][3],
-			'cantidad':respuesta[i][4],
-			'precio':respuesta[i][5],
-			'imagen':respuesta[i][6]}
-		product_list.append(item)
-
-	return jsonify(Productos=product_list)
+		return jsonify(Productos=product_list)
+	else:
+		return "Sesion expirada, inicie sesion"
 
 #Metodo para traer un solo producto por su id
 @app.route("/productos/<id>", methods=['GET'])
 def get_product(id):
+	sesion = request.headers['Sesion']
 	cur=mysql.connection.cursor()
-	cur.execute('SELECT * FROM producto where id=%s',(id))
-	respuesta= cur.fetchall() 
-	item={
-		'id':respuesta[0][0],
-		'id_categoria': respuesta[0][1],
-		'nombre':respuesta[0][2],
-		'descripcion':respuesta[0][3],
-		'cantidad':respuesta[0][4],
-		'precio':respuesta[0][5],
-		'imagen':respuesta[0][6]}
-		
-	return jsonify(Producto=item)
+	estado_sesion = cur.execute('SELECT * FROM token where token_cod= %s', [sesion])
+	cur.fetchall()
+	if estado_sesion==1:
+		cur.execute('SELECT * FROM producto where id=%s',(id))
+		respuesta= cur.fetchall() 
+		item={
+			'id':respuesta[0][0],
+			'id_categoria': respuesta[0][1],
+			'nombre':respuesta[0][2],
+			'descripcion':respuesta[0][3],
+			'cantidad':respuesta[0][4],
+			'precio':respuesta[0][5],
+			'imagen':respuesta[0][6]}
+			
+		return jsonify(Producto=item)
+	else:
+		return "Sesion expirada, inicie sesion"
 
 #Metodo para agregar un producto nuevo
 @app.route("/productos/",methods=['POST'])
 def add_producto():
 	
 	#Se busca el ultimo id en la base de datos para insertar el nuevo POST
+	sesion = request.headers['Sesion']
 	cur=mysql.connection.cursor()
-	cur.execute('SELECT max(id) from producto')
-	maxid=cur.fetchone()#El retorno es una tupla de la forma (max_id,)
-	
+	estado_sesion = cur.execute('SELECT * FROM token where token_cod= %s', [sesion])
+	cur.fetchall()
+	if estado_sesion==1:
+		cur.execute('SELECT max(id) from producto')
+		maxid=cur.fetchone()#El retorno es una tupla de la forma (max_id,)
+		
 
-	data=json.loads(request.data)#Se carga la data del json recibido
-	#Se llenan los datos del nuevo producto
-	newid=maxid[0]+1
-	nombre=data['nombre']
-	descripcion=data['descripcion']
-	cantidad=data['cantidad']
-	precio=data['precio']
-	imagen=data['imagen']
-	id_categoria=data['id_categoria']
-	
+		data=json.loads(request.data)#Se carga la data del json recibido
+		#Se llenan los datos del nuevo producto
+		newid=maxid[0]+1
+		nombre=data['nombre']
+		descripcion=data['descripcion']
+		cantidad=data['cantidad']
+		precio=data['precio']
+		imagen=data['imagen']
+		id_categoria=data['id_categoria']
+		
 
-	#se inserta el nuevo producto
-	cur.execute('INSERT INTO producto (id, nombre, descripcion, cantidad, precio, imagen, id_categoria) VALUES (%s, %s, %s, %s, %s, %s, %s)', (newid, nombre, descripcion, cantidad, precio, imagen, id_categoria ))
-	cur.connection.commit()
-	return "Done"
+		#se inserta el nuevo producto
+		cur.execute('INSERT INTO producto (id, nombre, descripcion, cantidad, precio, imagen, id_categoria) VALUES (%s, %s, %s, %s, %s, %s, %s)', (newid, nombre, descripcion, cantidad, precio, imagen, id_categoria ))
+		cur.connection.commit()
+		return "Done"
 
-	#Lista de todos los productos, se revisa si el post fue agregado (Opcional)
-	cur.execute('SELECT * FROM producto')
-	respuesta= cur.fetchall() 
-	product_list=[]
-	for i in range (0,len(respuesta)):
-		item={
-			'id':respuesta[0][0],
-			'nombre': respuesta[0][1],
-			'descripcion':respuesta[0][2],
-			'cantidad':respuesta[0][3],
-			'precio':respuesta[0][4],
-			'imagen':respuesta[0][5],
-			'id_categoria':respuesta[0][6]}
-		product_list.append(item)
-	return jsonify(Productos=product_list)
+		#Lista de todos los productos, se revisa si el post fue agregado (Opcional)
+		cur.execute('SELECT * FROM producto')
+		respuesta= cur.fetchall() 
+		product_list=[]
+		for i in range (0,len(respuesta)):
+			item={
+				'id':respuesta[0][0],
+				'nombre': respuesta[0][1],
+				'descripcion':respuesta[0][2],
+				'cantidad':respuesta[0][3],
+				'precio':respuesta[0][4],
+				'imagen':respuesta[0][5],
+				'id_categoria':respuesta[0][6]}
+			product_list.append(item)
+		return jsonify(Productos=product_list)
+	else:
+		return "Sesion expirada, inicie sesion"
 
 #Metodo para traer todos los usuarios 
 @app.route("/usuarios/", methods=['GET'])		
 def get_usuarios():
+	sesion = request.headers['Sesion']
 	cur=mysql.connection.cursor()
-	cur.execute('SELECT * FROM usuario')
-	respuesta= cur.fetchall() 
-	user_list=[]
-	for i in range (0,len(respuesta)):
-		item={
-			'id':respuesta[i][0],
-			'correo': respuesta[i][3],
-			'password':respuesta[i][4],
-			'nombre':respuesta[i][2],
-			'apellido':respuesta[i][2]}
-		user_list.append(item)
+	estado_sesion = cur.execute('SELECT * FROM token where token_cod= %s', [sesion])
+	cur.fetchall()
+	if estado_sesion==1:
+		cur.execute('SELECT * FROM usuario')
+		respuesta= cur.fetchall() 
+		user_list=[]
+		for i in range (0,len(respuesta)):
+			item={
+				'id':respuesta[i][0],
+				'correo': respuesta[i][3],
+				'password':respuesta[i][4],
+				'nombre':respuesta[i][2],
+				'apellido':respuesta[i][2]}
+			user_list.append(item)
 
-	return jsonify(Usuarios=user_list)
-
+		return jsonify(Usuarios=user_list)
+	else:
+		return "Sesion expirada, inicie sesion"
 #Metodo para traer un solo usuario por su id
 @app.route("/usuarios/<id>", methods=['GET'])
 def get_usuario(id):
+	sesion = request.headers['Sesion']
 	cur=mysql.connection.cursor()
-	cur.execute('SELECT * FROM usuario where id=%s',(id))
-	respuesta= cur.fetchall() 
-	item={
-			'id':respuesta[0][0],
-			'correo': respuesta[0][1],
-			'password':respuesta[0][2],
-			'nombre':respuesta[0][3],
-			'apellido':respuesta[0][4]}
-		
-	return jsonify(Usuario=item)
-
+	estado_sesion = cur.execute('SELECT * FROM token where token_cod= %s', [sesion])
+	cur.fetchall()
+	if estado_sesion==1:
+		cur.execute('SELECT * FROM usuario where id=%s',(id))
+		respuesta= cur.fetchall() 
+		item={
+				'id':respuesta[0][0],
+				'correo': respuesta[0][1],
+				'password':respuesta[0][2],
+				'nombre':respuesta[0][3],
+				'apellido':respuesta[0][4]}
+			
+		return jsonify(Usuario=item)
+	else:
+		return "Sesion expirada, inicie sesion"
 #Metodo para agregar un usuario nuevo
 @app.route("/usuarios/",methods=['POST'])
 def add_usuario():
 	
 	#Se busca el ultimo id en la base de datos para insertar el nuevo POST
+	sesion = request.headers['Sesion']
 	cur=mysql.connection.cursor()
-	cur.execute('SELECT max(id) from usuario')
-	maxid=cur.fetchone()#El retorno es una tupla de la forma (max_id,)
-	
-
-	data=json.loads(request.data)#Se carga la data del json recibido
-	#Se llenan los datos del nuevo usuario
-	newid=maxid[0]+1
-	nombre=data['nombre']
-	apellido=data['apellido']
-	correo=data['correo']
-	password=data['password']
-	
-
-	#se inserta el nuevo usuario
-	cur.execute('INSERT INTO usuario (id, nombre, apellido, correo, password) VALUES (%s, %s, %s, %s, %s)', (newid, nombre, apellido, correo, password ))
-	cur.connection.commit()
-	return "Done"
-
+	estado_sesion = cur.execute('SELECT * FROM token where token_cod= %s', [sesion])
+	cur.fetchall()
+	if estado_sesion==1:
+		cur.execute('SELECT max(id) from usuario')
+		maxid=cur.fetchone()#El retorno es una tupla de la forma (max_id,)
+		data=json.loads(request.data)#Se carga la data del json recibido
+		#Se llenan los datos del nuevo usuario
+		newid=maxid[0]+1
+		nombre=data['nombre']
+		apellido=data['apellido']
+		correo=data['correo']
+		password=data['password']
+		#se inserta el nuevo usuario
+		cur.execute('INSERT INTO usuario (id, nombre, apellido, correo, password) VALUES (%s, %s, %s, %s, %s)', (newid, nombre, apellido, correo, password ))
+		cur.connection.commit()
+		return "Done"
+	else:
+		return "Sesion expirada, inicie sesion"
 #Metodo para iniciar sesion
 @app.route("/usuarios/login",methods=['POST'])
 def login():
-	
 	cur=mysql.connection.cursor()
 	data=json.loads(request.data)#Se carga la data del json recibido
 	#Se llenan los datos del nuevo producto
 	correo=data['correo']
 	password=data['password']
-	
 	estado_query=cur.execute('SELECT * FROM usuario where correo= %s and password = %s', (correo, password))
 	respuesta= cur.fetchall() 
-
 	if estado_query==1:	
 		#OPCIONAL
 		item={
 			'correo': respuesta[0][1],
 			'password':respuesta[0][2]}
-		return  "Usuario encontrado"
+		token = uuid.uuid4();
+		cur.execute('INSERT INTO token (token_cod, correo) VALUES (%s, %s)', (token, correo))
+		cur.connection.commit()
+		return  jsonify(Sesion=token)
 	else:
 		return "Usuario o clave invalid@/s"
+
+
+@app.route("/usuarios/logout",methods=['POST'])
+def logout():
+	sesion = request.headers['Sesion']
+	cur=mysql.connection.cursor()
+	data=json.loads(request.data)
+	estado_sesion = cur.execute('SELECT * FROM token where token_cod= %s', [sesion])
+	cur.fetchall()
+	if estado_sesion==1:
+		cur.execute('DELETE FROM token WHERE token_cod = %s', [sesion])
+		cur.connection.commit()
+		return "Done"
+	else:
+		return "Sesion expirada, inicie sesion"
+
 
 @app.route('/')
 def index():
